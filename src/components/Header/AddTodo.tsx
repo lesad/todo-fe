54b351e7b3445086a6b32eaa9c95e3f44eec19dd
Adrián Leštinský <70895092+lesad@users.noreply.github.com>
2022-07-31 14:57:10 +0200

    feat: implemented functionality to add new todos

--- src/components/Header/AddTodo.tsx
+++ src/components/Header/AddTodo.tsx
@@ -1,11 +1,43 @@
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
+import { useAddTodoMutation } from '../../redux/apiSlice';
+
 const AddTodo = () => {
+  const [text, setText] = useState('');
+  const [addTodo] = useAddTodoMutation();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const trimmedText = e.target.value.trim();
+    if (e.key === 'Enter' && trimmedText) {
+      addTodo(trimmedText);
+      setText('');
+    }
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (text) {
+      addTodo(text);
+      setText('');
+    }
+  };
+
   return (
     <form className="flex flex-row my-3">
-      <input className="grow focus:outline-none" placeholder="New todo" />
-      <button>
+      <input
+        className="grow focus:outline-none"
+        placeholder="Enter new todo"
+        autoFocus
+        value={text}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleClick}>
         <FontAwesomeIcon icon={faPlus} />
       </button>
     </form>
