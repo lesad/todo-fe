4b683a71a9c42739971f40b4722152b903bf99d7
Adrián Leštinský <adrian.lestinsky@morosystems.cz>
2022-09-08 16:53:18 +0200

    Remove all default imports of React

--- src/components/Header/AddTodo.tsx
+++ src/components/Header/AddTodo.tsx
@@ -1,6 +1,6 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import { useState } from "react";
 
 import { useAddTodoMutation } from "../../redux/apiSlice";
 
