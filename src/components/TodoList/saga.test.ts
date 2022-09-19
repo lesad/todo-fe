037bc00100a3b88a84e3a6dd7e4feb3837311a8c
Adrián Leštinský <adrian.lestinsky@morosystems.cz>
2022-09-19 17:11:04 +0200

    Add saga tests

---
+++ src/components/TodoList/saga.test.ts
@@ -0,0 +1,34 @@
+import { put, takeEvery } from "redux-saga/effects";
+import { appendEffect } from "../../redux/actionsSlice";
+import { actions } from "./actions";
+import { storeLoadingFinished, storeLoadingStarted, watchLoading } from "./saga";
+
+describe("TodoList Saga", () => {
+    describe("watchLoading", () => {
+        const generator = watchLoading();
+
+        it("should wait for every LOADING_STARTED", () => {
+            expect(generator.next().value).toEqual(takeEvery(actions.LOADING_STARTED, storeLoadingStarted));
+        });
+
+        it("should wait for every LOADING_FINISHED", () => {
+            expect(generator.next().value).toEqual(takeEvery(actions.LOADING_FINISHED, storeLoadingFinished));
+        });
+    });
+
+    describe("storeLoadingStarted", () => {
+        const generator = storeLoadingStarted();
+
+        it("should put appendEffect", () => {
+            expect(generator.next().value).toEqual(put(appendEffect(actions.LOADING_STARTED)));
+        });
+    });
+
+    describe("storeLoadingFinished", () => {
+        const generator = storeLoadingFinished();
+
+        it("should put appendEffect", () => {
+            expect(generator.next().value).toEqual(put(appendEffect(actions.LOADING_FINISHED)));
+        });
+    });
+});
